59a3f506b1c9b87319457c5d3858e703c436f00c
Katherina-B <79216681+Katherina-B@users.noreply.github.com>
2023-10-20 18:06:13 +0200

    Update consumer.py

--- consumer.py
+++ consumer.py
@@ -1,35 +1,56 @@
-import asyncio
-import websockets
+from confluent_kafka import Consumer, KafkaException
 import json
 
-async def consumer():
-    url = "wss://ws.bitstamp.net/"
-    async with websockets.connect(url) as websocket:
-        channel_data = {
-            "event": "bts:subscribe",
-            "data": {
-                "channel": "live_orders_btcusd"
-            }
-        }
-        await websocket.send(json.dumps(channel_data))
+def consumer():
+    conf = {
+        'bootstrap.servers': 'localhost:9092',
+        'group.id': 'my_consumer_group',
+        'auto.offset.reset': 'earliest'
+    }
+
+    consumer = Consumer(conf)
+    topics = ['bitcoin_transactions'] 
+    try:
+        consumer.subscribe(topics)
+
         top_10_transactions = []
 
         while True:
-            message = await websocket.recv()
-            data = json.loads(message)
-            transaction = data.get('data')  # Use get to handle potential missing keys
+            msg = consumer.poll(timeout=1000)
+
+            if msg is None:
+                continue
+            if msg.error():
+                if msg.error().code() == KafkaException._PARTITION_EOF:
+                    print(f"Got end of partition event for {msg.topic()} [{msg.partition()}] at offset {msg.offset()}")
+                    continue
+                else:
+                    print(f"Error: {msg.error()}")
+                    break
+
+            # Розпакувати та обробити повідомлення
+            try:
+                data = json.loads(msg.value().decode('utf-8'))
+                transaction = data.get('data')
+            except Exception as e:
+                print(f"Error decoding message: {e}")
+                continue
 
             if transaction is not None:
-                print(transaction)  # Print the received data
+                #print(transaction)  # Вивести отримані дані
 
-                # Update top 10 transactions
+                # Оновити та вивести топ-10 транзакцій
                 top_10_transactions.append(transaction)
                 top_10_transactions = sorted(top_10_transactions, key=lambda x: x.get('price', 0), reverse=True)[:10]
 
-                # Print top 10 transactions
                 print('\nTop 10 Transactions:')
                 for idx, transaction in enumerate(top_10_transactions, start=1):
                     print(f'{idx}. Amount: {transaction.get("amount_str", "")}, Price: {transaction.get("price_str", "")}')
 
-# Run the consumer
-asyncio.get_event_loop().run_until_complete(consumer())
+    except KeyboardInterrupt:
+        pass
+    finally:
+        consumer.close()
+
+# Запустити консюмер
+consumer()
