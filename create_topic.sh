ee13a33e3bf366ce987512e19ddd7c1617c432b1
Katherina-B <79216681+Katherina-B@users.noreply.github.com>
2023-11-13 22:54:21 +0100

    Delete create_topic.sh

--- create_topic.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Викликаємо команду для створення топіка Kafka
-/usr/bin/kafka-topics --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic bitcoin_transaction
